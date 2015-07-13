357ad584313aa8ba9fa44c43534e03d455e6d8f6
Celeste Layne <layne.celeste@gmail.com>
2015-07-13 12:43:32 -0700

    enable CORS disable CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
+  skip_before_action :verify_authenticity_token,
+    except: [:create, :update, :delete]
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
