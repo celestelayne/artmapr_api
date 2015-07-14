bfd3f1a34e25bce1c4a9b8a3e9f36bba8edc9b99
Celeste Layne <layne.celeste@gmail.com>
2015-07-13 18:44:42 -0700

    sessions and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
     except: [:create, :update, :delete]
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include SessionsHelper
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+  def login(user)
+    session[:user_id] = user.id
+    @current_owner = user
+  end
+
+  def logout
+    @current_owner = session[:user_id] = nil
+  end
+
+  def current_owner
+    @current_owner ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_owner.nil?
+  end
+
+  def require_login
+    if !logged_in?
+      redirect_to '/login'
+  end
+
 end
