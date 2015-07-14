41b96df83a10dc5211e7378f025ecdea46875e19
Celeste Layne <layne.celeste@gmail.com>
2015-07-13 21:28:04 -0700

    session controller logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,31 @@
 class User < ActiveRecord::Base
+
   has_secure_password
+
+  before_validation :on => :create, :gen_api_token
+
+  validates :api_token,
+            :uniqueness => true
+
+  validates :email,
+            :presence => true,
+            :uniqueness => true
 
   def self.confirm(params)
     user = self.find_by!(email: params[:email])
     user.authenticate(params[:password])
   end
-  
+
+  def gen_api_token
+    token = SecureRandom.uuid
+
+    #run simple validation
+    until User.find_by(api_token: token).nil?
+      token = SecureRandom.uuid
+    end
+
+    self.api_token = token
+
+  end
+
 end
