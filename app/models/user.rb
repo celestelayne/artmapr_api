cf9e5989ce7b6128934150b534c52609bb531a4f
Celeste Layne <layne.celeste@gmail.com>
2015-07-21 15:50:42 -0700

    updated before validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   has_secure_password
 
-  before_validation :on => :create, :gen_api_token
+  before_validation(:on => :create){:gen_api_token}
 
   validates :api_token,
             uniqueness: true
@@ -15,9 +15,9 @@ class User < ActiveRecord::Base
   validates :email_confirmation,
             presence: true
 
-  # validates :first_name,
-  #           :last_name,
-  #           presence: true
+    # validates :first_name,
+    #           :last_name,
+    #           presence: true
 
   def self.confirm(params)
     # @user = User.find_by({email: params[:email]})
