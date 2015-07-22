80efce073373d781ca81a3e8d290b823fd418350
Celeste Layne <layne.celeste@gmail.com>
2015-07-21 22:13:45 -0700

    additional changes to api_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,45 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  first_name      :string
+#  last_name       :string
+#  email           :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  api_token       :string
+#
+
 class User < ActiveRecord::Base
 
-  before_save { self.email = email.downcase }
   has_secure_password
 
-  before_validation(:on => :create){:gen_api_token}
+  before_create :generate_api_token
+  before_save :check_and_set_token
 
-  validates :api_token,
-            uniqueness: true
-
-  validates :email,
-            presence: true,
-            uniqueness: true
-
-  validates :email_confirmation,
-            presence: true
-
-    # validates :first_name,
-    #           :last_name,
-    #           presence: true
+  validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email
 
   def self.confirm(params)
-    # @user = User.find_by({email: params[:email]})
-    # @user.try(:authenticate, params[:password])
     user = self.find_by!(email: params[:email])
     user.authenticate(params[:password])
   end
 
-  def gen_api_token
-    token = SecureRandom.uuid
+  #edge_case check for uniquness of randomized token
+  def generate_api_token
+    self.api_token = SecureRandom.uuid
+  end
 
-    #run simple validation
-    until User.find_by(api_token: token).nil?
-      token = SecureRandom.uuid
-    end
+  #User.find_each(&:save) this sets all tokens if not set
+  #this can be a rake task eventually
+  def check_and_set_token
+    generate_api_token if self.api_token.nil?
+  end
 
-    self.api_token = token
-
+  #to be filled in (mailer needed or something plus background process)
+  def send_email_confirmation
   end
 
 end
